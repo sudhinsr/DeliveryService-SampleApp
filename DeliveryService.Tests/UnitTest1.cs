557e51872df98f5318c17ca398d1133caf40e154
Sudhin <sudhinsureshr@gmail.com>
2019-02-12 21:37:20 +0530

    Add project files.

---
+++ DeliveryService.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeliveryService.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
